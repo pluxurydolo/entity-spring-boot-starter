ba202cacc18d86cd953d650732d73cf78d8ef1ce
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-01-11 15:28:20 +0300

    11.01.2026

--- src/main/java/com/pluxurydolo/entity/config/EntityAutoConfiguration.java
+++ src/main/java/com/pluxurydolo/entity/config/EntityAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.pluxurydolo.entity.config;
 
 import com.pluxurydolo.entity.utils.UUIDGenerator;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernatePropertiesCustomizer;
 import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
@@ -10,5 +11,14 @@ public class EntityAutoConfiguration {
     @Bean
     public UUIDGenerator uuidGenerator() {
         return new UUIDGenerator();
+    }
+
+    @Bean
+    public HibernatePropertiesCustomizer ormXmlMappingCustomizer() {
+        return properties -> {
+            properties.put("hibernate.jpa.compliance.proxy", true);
+            properties.put("hibernate.jpa.compliance.query", true);
+            properties.put("jakarta.persistence.validation.mode", "NONE");
+        };
     }
 }
